066be3d5d9286277aac4cb1e123861ab28cba8d5
tejasgawali <tejas.gawali4@gmail.com>
2017-09-24 15:22:22 +0530

    Few Check while Geting Permissions

--- app/src/main/java/in/techdrop/tejas/gpsexample/GetUserLocation.java
+++ app/src/main/java/in/techdrop/tejas/gpsexample/GetUserLocation.java
@@ -3,6 +3,8 @@ package in.techdrop.tejas.gpsexample;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -13,14 +15,18 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class GetUserLocation extends FragmentActivity implements OnMapReadyCallback
 {
@@ -43,19 +49,22 @@ public class GetUserLocation extends FragmentActivity implements OnMapReadyCallb
   public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
     super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-      if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-        // TODO: Consider calling
-        //    ActivityCompat#requestPermissions
-        // here to request the missing permissions, and then overriding
-        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-        //                                          int[] grantResults)
-        // to handle the case where the user grants the permission. See the documentation
-        // for ActivityCompat#requestPermissions for more details.
-        return;
+    if (requestCode == 1) {
+
+      if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+          {
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+          }
+
+        }
+
       }
-      locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, locationListener);
-     }
+
+    }
 
   }
 
@@ -70,13 +79,63 @@ public class GetUserLocation extends FragmentActivity implements OnMapReadyCallb
       @Override
       public void onLocationChanged(Location location) {
 
-        Log.i("Location", location.toString());
-        //Toast.makeText(getApplicationContext(),location.toString(),Toast.LENGTH_SHORT).show();
+        LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
 
-        LatLng UserLocation = new LatLng(location.getLatitude(), location.getLongitude());
         mMap.clear();
-        mMap.addMarker(new MarkerOptions().position(UserLocation).title("Your Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UserLocation,10));
+        mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        try {
+
+          List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+          if (listAddresses != null && listAddresses.size() > 0) {
+
+            Log.i("PlaceInfo", listAddresses.get(0).toString());
+
+            String address = "";
+
+            if (listAddresses.get(0).getSubThoroughfare() != null) {
+
+              address += listAddresses.get(0).getSubThoroughfare() + " ";
+
+            }
+
+            if (listAddresses.get(0).getThoroughfare() != null) {
+
+              address += listAddresses.get(0).getThoroughfare() + ", ";
+
+            }
+
+            if (listAddresses.get(0).getLocality() != null) {
+
+              address += listAddresses.get(0).getLocality() + ", ";
+
+            }
+
+            if (listAddresses.get(0).getPostalCode() != null) {
+
+              address += listAddresses.get(0).getPostalCode() + ", ";
+
+            }
+
+            if (listAddresses.get(0).getCountryName() != null) {
+
+              address += listAddresses.get(0).getCountryName();
+
+            }
+
+            Toast.makeText(GetUserLocation.this, address, Toast.LENGTH_SHORT).show();
+
+          }
+
+        } catch (IOException e) {
+
+          e.printStackTrace();
+
+        }
 
       }
 
@@ -99,10 +158,18 @@ public class GetUserLocation extends FragmentActivity implements OnMapReadyCallb
     if (Build.VERSION.SDK_INT < 23)
     {
       locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+      Location lastKnownLocation = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+      LatLng UserLocation = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+
+      mMap.clear();
+      mMap.addMarker(new MarkerOptions().position(UserLocation).title("Your Location"));
+      mMap.moveCamera(CameraUpdateFactory.newLatLng(UserLocation));
     }
     else
     {
-      if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+      if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+      {
         //Ask for Persmission
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
@@ -112,11 +179,11 @@ public class GetUserLocation extends FragmentActivity implements OnMapReadyCallb
         locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, locationListener);
 
         Location lastKnownLocation = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
-
         LatLng UserLocation = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+
         mMap.clear();
-        mMap.addMarker(new MarkerOptions().position(UserLocation).title("Your Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(UserLocation,10));
+        mMap.addMarker(new MarkerOptions().position(UserLocation).title("Your Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(UserLocation));
       }
     }
 
